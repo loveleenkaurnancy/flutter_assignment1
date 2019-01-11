f468785ec3cb8490179255435acf500a5955911f
loveleen <loveleen.nancy@gmail.com>
2019-01-11 12:19:01 +0530

    Assignment Completed

---
+++ lib/text_control.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import './text_output.dart';
+
+class TextControl extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return _TextControlState();
+    }
+}
+
+class _TextControlState extends State<TextControl> {
+  String _mainText = 'Assignment 1';
+
+  @override
+    Widget build(BuildContext context) {
+      return Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _mainText = 'This changed';
+                });
+              },
+              child: Text('Change Text'),
+            ),
+            TextOutput(_mainText)
+          ],
+        );
+    }
+}
