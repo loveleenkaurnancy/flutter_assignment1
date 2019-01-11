332ccfb5e874fa72719848e17defb5999a37ceaa
loveleen <loveleen.nancy@gmail.com>
2019-01-11 11:36:59 +0530

    Main Method

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@
 
 import 'package:flutter/material.dart';
 
+main() {
+    runApp(MyApp());
+  }
+  
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
